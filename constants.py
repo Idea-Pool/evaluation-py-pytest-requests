44627af8cdbc5356394c29ae974be371c761fab3
Milan_Horvath <Milan.Horvath@wolterskluwer.com>
2020-12-16 11:57:19 +0100

    init commit

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+# The API KEY should be available, after registering on the themoviedb.org webpage
 API_KEY = {YOUR_API_KEY}
 API_URL = "https://api.themoviedb.org/3"
 GUEST_SESSION = "/authentication/guest_session/"
